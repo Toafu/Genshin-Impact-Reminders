46604b1c69e278b16119b2a232ffd93717530dba
rwchua <71936834+rwchua@users.noreply.github.com>
2021-05-13 08:02:08 -0500

    Delete bruh.js

--- bruh.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	commands: 'bruh', //Can include all aliases of a command
-	minArgs: 0,
-	maxArgs: 0,
-	callback: message => {
-		message.channel.send('This is a certified bruh moment™');
-	},
-};
