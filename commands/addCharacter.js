21975970b8b3f93365b99cf0a25ac3682f939610
rwchua <rwchua@umich.edu>
2021-05-17 21:54:49 -0500

    Add All Characters

--- commands/addCharacter.js
+++ commands/addCharacter.js
@@ -21,39 +21,66 @@ module.exports = {
 		} else if (message.content.startsWith('b!track ')) {
 			query = message.content.replace('b!track ', '').toLowerCase();
 		}
-		const querytest = Number(query);
-		if (Number.isNaN(querytest) === true) {
-			index = characters.findIndex(person => person.name.toLowerCase() === query);
-		} else {
-			index = querytest;
-		}
 
-		if(index >= 0 && index < characters.length) {
+		if (query === 'all') {
 			await mongo().then(async mongoose => {
 				try {
 					await savedCharacterSchema.findOneAndUpdate({
 						_id: id,
 					}, {
-						$addToSet: { savedCharacters: characters[index] },
+						$addToSet: { savedCharacters: characters },
 					}, {
 						upsert: true,
-					}).exec();
+					});
 				} finally {
 					mongoose.connection.close();
 				}
 			});
-			const embed = new Discord.MessageEmbed()
+			const addallcharsembed = new Discord.MessageEmbed()
 				.setColor('#00FF97')
 				.setAuthor(message.author.username)
 				.addFields(
 					{
-						name: 'Adding Character',
-						value: `You are now tracking **${characters[index].name}** ${getEmotes.getEmote(characters[index].element)}`,
+						name: 'Tracking All Characters',
+						value: 'I hope you realized what you just did.',
 						inline: true,
 					});
-			message.channel.send(embed);
+			message.channel.send(addallcharsembed);
 		} else {
-			message.channel.send(`Please use a valid ID [\`0-${characters.length - 1}\`] or character name.`);
+			const querytest = Number(query);
+			if (Number.isNaN(querytest) === true) {
+				index = characters.findIndex(person => person.name.toLowerCase() === query);
+			} else {
+				index = querytest;
+			}
+
+			if(index >= 0 && index < characters.length) {
+				await mongo().then(async mongoose => {
+					try {
+						await savedCharacterSchema.findOneAndUpdate({
+							_id: id,
+						}, {
+							$addToSet: { savedCharacters: characters[index] },
+						}, {
+							upsert: true,
+						}).exec();
+					} finally {
+						mongoose.connection.close();
+					}
+				});
+				const embed = new Discord.MessageEmbed()
+					.setColor('#00FF97')
+					.setAuthor(message.author.username)
+					.addFields(
+						{
+							name: 'Adding Character',
+							value: `You are now tracking **${characters[index].name}** ${getEmotes.getEmote(characters[index].element)}`,
+							inline: true,
+						});
+				message.channel.send(embed);
+			} else {
+				message.channel.send(`Please use a valid ID [\`0-${characters.length - 1}\`] or character name.`);
+			}
 		}
 	},
 };
