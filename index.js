ba0979e1a88e3700cf9836e8a234e4b534e6afca
rwchua <rwchua@umich.edu>
2021-05-19 14:30:05 -0500

    Improved command load time

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 require('module-alias/register');
 
-const path = require('path');
-const fs = require('fs');
+
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
 //const config = require('@root/config.json');
+const loadCommands = require('./commands/load-commands');
 const command = require('./command');
 const mongo = require('./mongo');
 
@@ -22,25 +22,7 @@ client.on('ready', async () => {
 		}
 	});
 
-	const baseFile = 'command-base.js';
-	const commandBase = require(`./commands/${baseFile}`);
-
-	const readCommands = dir => {
-		const files = fs.readdirSync(path.join(__dirname, dir));
-		for (const file of files) {
-			const stat = fs.lstatSync(path.join(__dirname, dir, file));
-			if (stat.isDirectory()) {
-				readCommands(path.join(dir, file));
-			} else if (file !== baseFile) {
-				const option = require(path.join(__dirname, dir, file));
-				commandBase(option);
-			}
-		}
-	};
-
-	readCommands('commands');
-
-	commandBase.listen(client);
+	loadCommands(client);
 
 	client.user.setPresence({
 		activity: {
