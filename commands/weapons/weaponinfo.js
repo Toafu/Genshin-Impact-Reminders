5eb2a660f3563cf8ca0693b9e0377efaffbfa996
rwchua <rwchua@umich.edu>
2021-05-20 20:34:02 -0500

    Apostrophe fix 3

--- commands/weapons/weaponinfo.js
+++ commands/weapons/weaponinfo.js
@@ -12,7 +12,7 @@ module.exports = {
 	callback: (message, arguments, text) => {
 		let index;
 		const query = text.toLowerCase();
-		query.replace(/’/g, '\'');
+		query.replace(/[’‘]/g, '\'');
 
 		const querytest = Number(query);
 		if (Number.isNaN(querytest) === true) {
