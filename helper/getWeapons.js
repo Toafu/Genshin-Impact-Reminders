814ae61e780d734765a831ada7c0b6f16056607a
rwchua <rwchua@umich.edu>
2021-06-14 11:49:43 -0500

    Added weapon images

--- helper/getWeapons.js
+++ helper/getWeapons.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-const weaponRead = (name, type, stars, id, passive, stat, mat, days, loot1, loot2, obtain, location) => {
+const weaponRead = (name, type, stars, id, passive, stat, mat, days, loot1, loot2, obtain, img, location) => {
 	return {
 		name,
 		type,
@@ -13,6 +13,7 @@ const weaponRead = (name, type, stars, id, passive, stat, mat, days, loot1, loot
 		loot1,
 		loot2,
 		obtain,
+		img,
 		location,
 	};
 };
@@ -24,8 +25,8 @@ const getWeapons = () => {
 		const word = text.split(/[\n\t\r]+/);
 		let i = 0;
 		while(word.length > 0) {
-			weapons[i] = (weaponRead(word[0], word[1], word[2], i, word[3], word[4], word[5], word[6], word[7], word[8], word[9], word[10]));
-			word.splice(0, 11);
+			weapons[i] = (weaponRead(word[0], word[1], word[2], i, word[3], word[4], word[5], word[6], word[7], word[8], word[9], word[10], word[11]));
+			word.splice(0, 12);
 			i += 1;
 		}
 		weapons.forEach(weapon => {
@@ -33,7 +34,7 @@ const getWeapons = () => {
 				weapon.passive = weapon.passive.substring(1, weapon.passive.length - 1);
 			}
 		});
-		//console.log(weapons[89]);
+		//console.log(weapons[34]);
 	});
 	return weapons;
 };
