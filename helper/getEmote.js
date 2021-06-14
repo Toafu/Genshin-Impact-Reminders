0f017f9b0b1ff43b111136815e6460adbda52366
rwchua <rwchua@umich.edu>
2021-06-14 13:57:34 -0500

    Added weekly boss materials to charinfo

--- helper/getEmote.js
+++ helper/getEmote.js
@@ -12,6 +12,18 @@ const emotes = {
 	Guyun: '<:Guyun:844756143724691536>',
 	Elixir: '<:Elixir:844756143431614485>',
 	Aerosiderite: '<:Aerosiderite:844756142752137286>',
+	Sigh: '<:Sigh:854063994989838386>',
+	Plume: '<:Plume:854063994830848010>',
+	Claw: '<:Claw:854063994666090516>',
+	Locket: '<:Locket:854063994716291092>',
+	Tail: '<:Tail:854063994871873606>',
+	Ring: '<:Ring:854063994885505074>',
+	Tusk: '<:Tusk:854063994960871465>',
+	Shadow: '<:Shadow:854063994813284412>',
+	Shard: '<:Shard:854063994829537290>',
+	Branch: '<:Branch:854063993761169418>',
+	Crown: '<:Crown:854063994352304179>',
+	Scale: '<:Scale:854063994742898758>',
 };
 
 const getEmote = string => {
@@ -61,6 +73,51 @@ const getEmote = string => {
 			break;
 		case 'Aerosiderite':
 			words[i] = `Aerosiderite ${emotes.Aerosiderite}`;
+			break;
+		case 'Dvalin\'s':
+			if (words[i + 1] === 'Sigh') {
+				words[i + 1] = `Sighs ${emotes.Sigh}`;
+			} else if (words[i + 1] === 'Plumes') {
+				words[i + 1] = `Plume ${emotes.Plume}`;
+			} else if (words[i + 1] === 'Claw') {
+				words[i + 1] = `Claws ${emotes.Claw}`;
+			}
+			break;
+		case 'Ring':
+			words[i] = `Rings of Boreas ${emotes.Ring}`;
+			words.splice(i + 1, 2);
+			break;
+		case 'Spirit':
+			words[i] = `Spirit Lockets of Boreas ${emotes.Locket}`;
+			words.splice(i + 1, 3);
+			break;
+		case 'Tail':
+			words[i] = `Tails of Boreas ${emotes.Tail}`;
+			words.splice(i + 1, 2);
+			break;
+		case 'Tusk':
+			words[i] = `Tusks of Monoceros Caeli ${emotes.Tusk}`;
+			words.splice(i + 1, 3);
+			break;
+		case 'Shard':
+			words[i] = `Shards of a Foul Legacy ${emotes.Shard}`;
+			words.splice(i + 1, 4);
+			break;
+		case 'Shadow':
+			words[i] = `Shadows of the Warrior ${emotes.Shadow}`;
+			words.splice(i + 1, 3);
+			break;
+		case 'Dragon':
+			words[i] = `Dragon Lord's Crowns ${emotes.Crown}`;
+			words.splice(i + 1, 2);
+			break;
+		case 'Bloodjade':
+			words[i] = `Bloodjade Branches ${emotes.Branch}`;
+			words.splice(i + 1, 1);
+			break;
+		case 'Gilded':
+			words[i] = `Gilded Scales ${emotes.Scale}`;
+			words.splice(i + 1, 1);
 			break;
 		}
 	}
