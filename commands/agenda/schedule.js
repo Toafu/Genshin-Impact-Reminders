25f78f552b23e5353ff4023b352def038af3f344
Toafu <ToafdaLoaf@gmail.com>
2022-09-17 15:11:33 -0400

    Actually unbloating logs

--- commands/agenda/schedule.js
+++ commands/agenda/schedule.js
@@ -25,7 +25,7 @@ module.exports = {
 				'date.minute': now.getMinutes(),
 			};
 
-			console.log(`Searching for scheduled agenda at ${now.getHours()}:${now.getMinutes()}`);
+			//console.log(`Searching for scheduled agenda at ${now.getHours()}:${now.getMinutes()}`);
 
 			const result = await scheduleSchema.find(query);
 
